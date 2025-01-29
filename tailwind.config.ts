2bdae9c1ee62378b90c6ee95f1bb034fa090ffa7
meetqy <meetqy@qq.com>
2025-01-29 18:48:29 +0800

    add HeroUI integration and update Tailwind configuration; remove unused post component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
+import { heroui } from "@heroui/react";
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.tsx",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +14,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
