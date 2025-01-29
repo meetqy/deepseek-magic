bc212b7d63a6e66943adc0f99de7666122dcb476
meetqy <meetqy@qq.com>
2025-01-29 18:53:10 +0800

    add internationalization support with next-intl; configure routing and middleware

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,34 @@
+import "~/styles/globals.css";
+import { GeistSans } from "geist/font/sans";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "~/i18n/routing";
+import { TRPCReactProvider } from "~/trpc/react";
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as never)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} className={`${GeistSans.variable}`}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
