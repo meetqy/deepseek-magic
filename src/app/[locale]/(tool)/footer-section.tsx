794bb982df5750d6115dfc525491d8c2d2729d5c
meetqy <meetqy@qq.com>
2025-02-03 13:53:39 +0800

    feat: enhance footer section with a divider and related link heading

--- src/app/[locale]/(tool)/footer-section.tsx
+++ src/app/[locale]/(tool)/footer-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@heroui/react";
+import { Button, Divider } from "@heroui/react";
 import { Icon } from "@iconify/react";
 import { Link, usePathname } from "~/i18n/routing";
 import { siteConfig } from "~/lib/site-config";
@@ -9,19 +9,23 @@ export const FooterSection = () => {
   const pathname = usePathname();
 
   return (
-    <section className="container mt-20">
-      <Button
-        isIconOnly
-        as={Link}
-        href={
-          siteConfig.github +
-          "/tree/main/src/app/%5Blocale%5D/(tool)" +
-          pathname
-        }
-        target="_blank"
-      >
-        <Icon className="h-6 w-6" icon="mdi:github" />
-      </Button>
-    </section>
+    <>
+      <Divider className="my-12" />
+      <section className="prose mx-auto max-w-screen-xl">
+        <h2>Related link</h2>
+        <Button
+          isIconOnly
+          as={Link}
+          href={
+            siteConfig.github +
+            "/tree/main/src/app/%5Blocale%5D/(tool)" +
+            pathname
+          }
+          target="_blank"
+        >
+          <Icon className="h-6 w-6" icon="mdi:github" />
+        </Button>
+      </section>
+    </>
   );
 };
